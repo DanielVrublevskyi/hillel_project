5aca0f7a27491a3bff9960e0e9fecc1efd474911
Daniel <70908919+DanielVrublevskyi@users.noreply.github.com>
2024-11-15 20:46:01 +0200

    Added variables

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -2,5 +2,13 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("hi");
+        int number = 10;
+        String text = "text";
+        double d = 45.6;
+        float f = 58585f;
+        long l = 3455555L;
+
+        char c = 't';
+        byte b = 's';
     }
 }
